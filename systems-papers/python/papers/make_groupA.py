0ddf844ce81a7590e6faa87d95df242c732b07eb
Henry <henry@reed.edu>
2018-07-19 19:54:59 -0700

    fixes

--- systems-papers/python/papers/make_groupA.py
+++ systems-papers/python/papers/make_groupA.py
@@ -5,11 +5,15 @@ import utils.xml as xml
 
 total_citations_count = 0
 
-for fn in paper_data.getPaperFilenames_XML():
+papers_filenames = paper_data.getPaperFilenames_XML()
+print("len(papers) =",papers_filenames)
+
+for fn in papers_filenames:
     root = xml.parseXML(fn)
     try:
         reflist = xml.getDescendantByTagPath(root, ["back","ref-list"])
         total_citations_count += len(reflist)
     except:
+        pass
 
 print("total citations count:", total_citations_count)
