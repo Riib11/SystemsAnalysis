e2532768f297523a94e231345d24316519bf04fe
Henry <henry@reed.edu>
2018-07-19 19:49:50 -0700

    updated python/papers

--- systems-papers/python/utils/data.py
+++ systems-papers/python/utils/data.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+# ON GIT
+
 systems_papers_directory = "../../systems-papers/"
 
 data_directory     = systems_papers_directory+"authors/data/"
@@ -11,8 +13,20 @@ features_directory = systems_papers_directory+"authors/features/"
 
 papers_directory   = systems_papers_directory + "sys-papers/"
 
-semantic_scholar_dir = "/data/sys-papers/semsch/"
+# OFF GIT
+
+paperdata_directory = "/data/sys-papers/"
+
+semantic_scholar_dir = paperdata_directory + "semsch/"
 semantic_scholar_proccessed_dir = systems_papers_directory + "semantic-scholar/"
+
+papergroups_directory = paperdata_directory + "papers/"
+groupA_directory = papergroups_directory + "A/"
+groupB_directory = papergroups_directory + "B/"
+
+#
+#
+#
 
 def fileToString(path):
     try:
@@ -21,7 +35,7 @@ def fileToString(path):
             for line in file: s += line
             return s
     except:
-        print("[!] Problem reading file at: " + path)
+        print("[!] Problem reading file: " + path)
 
 def getJSONData(path):
     data_raw = fileToString(path)
