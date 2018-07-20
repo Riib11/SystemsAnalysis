7f51de98c5a72682e8af20ad6fcf61088f761841
Henry <henry@reed.edu>
2018-07-19 20:03:23 -0700

    updates

--- systems-papers/python/papers/make_groupA.py
+++ systems-papers/python/papers/make_groupA.py
@@ -1,21 +1,24 @@
 import xml.etree.ElementTree as ET
 import os
+from tqdm import tqdm
 
 import papers.paper_data as paper_data
 import utils.data as data
 import utils.xml as xml
 
 total_citations_count = 0
+total_errors_count = 0
 
 papers_filenames = paper_data.getPaperFilenames_XML()
 print("len(papers) =",papers_filenames)
 
-for fn in papers_filenames:
+for fn in tqdm(papers_filenames):
     root = xml.parseXML(fn)
     try:
         reflist = xml.getDescendantByTagPath(root, ["back","ref-list"])
         total_citations_count += len(reflist)
     except:
-        pass
+        total_errors_count += 1
 
 print("total citations count:", total_citations_count)
+print("total errors count:", total_errors_count)
