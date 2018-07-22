f4a3dd2cfc24c365afe560b0744dfd3ecaa95bdf
Henry <henry@reed.edu>
2018-07-21 21:27:02 -0700

    updates

--- systems-papers/python/semantic_scholar/make_grepciters.py
+++ systems-papers/python/semantic_scholar/make_grepciters.py
@@ -10,18 +10,18 @@ grepciters = open(fn_grepciters,"w+")
 
 # loop through titles of papers
 cfns = [ fn for fn in u_data.getConferenceFilenames() ]
-last_i = len(cfns)-1
+last_j = 0 # len(cfns)-1
 
 # start index
-i = 43
+i = 0
 
 
 cfns = cfns[i:] # already did the first 43
 
-per_part = 8                        # processer per partition
-parts = math.ceil(len(cfns)/per_part)  # number of partitions
-part_i = 0                          # index of partition
-part_j = 0                          # index within partition
+per_part = 8                            # processer per partition
+parts = math.ceil(len(cfns)/per_part)   # number of partitions
+part_i = 0                              # index within partition
+part_j = 0                              # index of partition
 
 
 for cfn in tqdm(cfns):
@@ -30,20 +30,31 @@ for cfn in tqdm(cfns):
     data = u_data.getPapers(cfn)
     papers = data["papers"]
 
-    cmd = ''
+    cmd = '#!/bin/bash' + '\n'
 
     if part_i == 0:
-        cmd += '\n\nnohup echo "starting section ' + str(part_j) + '"'
-        cmd += ' & grep -h'
+        cmd += 'echo "starting section ' + str(part_j) + '"'
+        cmd += ' & grep -i -h'
     else:
-        cmd += ' & grep -h'
+        cmd += ' & grep -i -h'
 
     for p in papers:
         title = p["title"].replace('"','\\"')
-        cmd += ' -e "' + title + '"'
+        cmd += " -e '\"id\":\"" + title + "\"'"
 
     cmd += ' ' + u_data.semantic_scholar_dir + 's2-corpus-*.json'
-    cmd += ' > ' + s2data.makeCitersCnfFn(i)
+
+    datafile_fn = s2data.makeCitersCnfFn(i)
+
+    # write finds to file
+    cmd += ' > ' + datafile_fn
+
+    # log the files that have problems
+    cmd += '\n'
+    cmd += "count=$(wc -l " + datafile_fn + " )"
+    cmd += "target='" + str(len(papers)) + "'"
+    cmd += "if [ $count != $target ]; then "
+    cmd += "echo " + datafile_fn + " has an incorrect number of results > " + datafile_fn+"_errors " + "\nfi\n"
 
     # write cmd
     grepciters.write(cmd)
@@ -51,11 +62,9 @@ for cfn in tqdm(cfns):
     # increment
     i      += 1
     part_i += 1
+    if part_j == last_j: break
     if part_i == per_part:
         part_i = 0
         part_j += 1
-
-grepciters.write(
-    '\n\necho "[Done] Don\'t forget to concat the output and format it and everything!"')
 
 grepciters.close()
