10a5ddf6bff0af3373dc710d812f146a7dd5021e
Henry <henry@reed.edu>
2018-08-14 15:53:42 -0700

    updates

--- systems-papers/python/network_citation/papers_statistics.py
+++ systems-papers/python/network_citation/papers_statistics.py
@@ -11,104 +11,101 @@ from tqdm import tqdm
 import utils.data as u_data
 import semantic_scholar.s2data as s2data
 
-def calcuate():
 
-    ################################################################
-    print("[#] Loading Data:")
-    
-    gA = s2data.get_dict_gA()
-    gB = s2data.get_dict_gB()
+################################################################
+print("[#] Loading Data:")
 
-    ################################################################
-    print("[#] Analyzing gA Data:")
+gA = s2data.get_dict_gA()
+gB = s2data.get_dict_gB()
 
-    def increment_entry(d, k):
-        k = k.lower()
-        if not k in d: d[k] = 0
-        d[k] += 1
+################################################################
+print("[#] Analyzing gA Data:")
 
-    gA_statistics = {
-        "outcitationcounts": [],
-        "titlewords": {}
-    }
-     
-    for paper in tqdm(gA.values()):
-        # citation count
-        outcitationcount = len(paper["outCitations"])
-        gA_statistics["outcitationcounts"].append(outcitationcount)
-        # title words
-        titlewords = paper["title"].split()
-        for word in titlewords:
-            increment_entry(gA_statistics["titlewords"],word)
+def increment_entry(d, k):
+    k = k.lower()
+    if not k in d: d[k] = 0
+    d[k] += 1
 
-    ################################################################
-    print("[#] Analyzing gA Data:")
+gA_statistics = {
+    "outcitationcounts": [],
+    "titlewords": {}
+}
+ 
+for paper in tqdm(gA.values()):
+    # citation count
+    outcitationcount = len(paper["outCitations"])
+    gA_statistics["outcitationcounts"].append(outcitationcount)
+    # title words
+    titlewords = paper["title"].split()
+    for word in titlewords:
+        increment_entry(gA_statistics["titlewords"],word)
 
-    gB_statistics = {
-        "incitationcounts": [],
-        "titlewords": {},
-        "years": []
-    }
+################################################################
+print("[#] Analyzing gA Data:")
 
-    for paper in tqdm(gB.values()):
-        # citation count
-        incitationcount = len(paper["inCitations"])
-        gB_statistics["incitationcounts"].append(incitationcount)
-        # title words
-        titlewords = paper["title"].split()
-        for word in titlewords:
-            increment_entry(gB_statistics["titlewords"],word)
-        # years
-        if "year" in paper:
-            gB_statistics["years"].append(paper["year"])
+gB_statistics = {
+    "incitationcounts": [],
+    "titlewords": {},
+    "years": []
+}
 
-    ################################################################
-    print("[#] Writing Results:")
+for paper in tqdm(gB.values()):
+    # citation count
+    incitationcount = len(paper["inCitations"])
+    gB_statistics["incitationcounts"].append(incitationcount)
+    # title words
+    titlewords = paper["title"].split()
+    for word in titlewords:
+        increment_entry(gB_statistics["titlewords"],word)
+    # years
+    if "year" in paper:
+        gB_statistics["years"].append(paper["year"])
 
-    with open("papers_statistics.txt","w+") as file:
-        #
-        #
-        file.write("################################\n")
-        file.write("gA statistics:\n")
-        #
-        file.write("  + outCitation counts:\n")
-        file.write("  |     mean : "+str(
-            np.mean(gA_statistics["outcitationcounts"]))+"\n")
-        file.write("  |   median : "+str(
-            np.median(gA_statistics["outcitationcounts"]))+"\n")
-        file.write("  |      std : "+str(
-            np.std(gA_statistics["outcitationcounts"]))+"\n")
-        #
-        file.write("  + top couple title words:\n")
-        def get(i): return i[1]
-        titlewords = sorted(list(gA_statistics["titlewords"].items()), key=get)
-        titlewords_len = len(titlewords)
-        for i in range(1,100+1): file.write("  |   "+str(i)+". ["+str(np.round(titlewords[-i][1]/titlewords_len, 3))+"] "+titlewords[-i][0]+"\n")
+################################################################
+print("[#] Writing Results:")
 
-        #
-        #
-        file.write("################################\n")
-        file.write("gB statistics:\n")
-        #        
-        file.write("  + inCitation counts:\n")
-        file.write("  |     mean : "+str(
-            np.mean(gB_statistics["incitationcounts"]))+"\n")
-        file.write("  |   median : "+str(
-            np.median(gB_statistics["incitationcounts"]))+"\n")
-        file.write("  |      std : "+str(
-            np.std(gB_statistics["incitationcounts"]))+"\n")
-        #
-        file.write("  + top couple title words:\n")
-        titlewords = sorted(list(gB_statistics["titlewords"].items()), key=get)
-        titlewords_len = len(titlewords)
-        for i in range(1,100+1): file.write("  |   "+str(i)+". ["+str(np.round(titlewords[-i][1]/titlewords_len, 3))+"] "+titlewords[-i][0]+"\n")
-        #
-        file.write("  + publish years:\n")
-        file.write("  |     mean : "+str(
-            np.mean(gB_statistics["years"]))+"\n")
-        file.write("  |   median : "+str(
-            np.median(gB_statistics["years"]))+"\n")
-        file.write("  |      std : "+str(
-            np.std(gB_statistics["years"]))+"\n")
+with open("../statistics/papers_statistics.txt","w+") as file:
+    #
+    #
+    file.write("################################\n")
+    file.write("gA statistics:\n")
+    #
+    file.write("  + outCitation counts:\n")
+    file.write("  |     mean : "+str(
+        np.mean(gA_statistics["outcitationcounts"]))+"\n")
+    file.write("  |   median : "+str(
+        np.median(gA_statistics["outcitationcounts"]))+"\n")
+    file.write("  |      std : "+str(
+        np.std(gA_statistics["outcitationcounts"]))+"\n")
+    #
+    file.write("  + top couple title words:\n")
+    def get(i): return i[1]
+    titlewords = sorted(list(gA_statistics["titlewords"].items()), key=get)
+    titlewords_len = len(titlewords)
+    for i in range(1,100+1): file.write("  |   "+str(i)+". ["+str(np.round(titlewords[-i][1]/titlewords_len, 3))+"] "+titlewords[-i][0]+"\n")
 
-calcuate()
+    #
+    #
+    file.write("################################\n")
+    file.write("gB statistics:\n")
+    #        
+    file.write("  + inCitation counts:\n")
+    file.write("  |     mean : "+str(
+        np.mean(gB_statistics["incitationcounts"]))+"\n")
+    file.write("  |   median : "+str(
+        np.median(gB_statistics["incitationcounts"]))+"\n")
+    file.write("  |      std : "+str(
+        np.std(gB_statistics["incitationcounts"]))+"\n")
+    #
+    file.write("  + top couple title words:\n")
+    titlewords = sorted(list(gB_statistics["titlewords"].items()), key=get)
+    titlewords_len = len(titlewords)
+    for i in range(1,100+1): file.write("  |   "+str(i)+". ["+str(np.round(titlewords[-i][1]/titlewords_len, 3))+"] "+titlewords[-i][0]+"\n")
+    #
+    file.write("  + publish years:\n")
+    file.write("  |     mean : "+str(
+        np.mean(gB_statistics["years"]))+"\n")
+    file.write("  |   median : "+str(
+        np.median(gB_statistics["years"]))+"\n")
+    file.write("  |      std : "+str(
+        np.std(gB_statistics["years"]))+"\n")
---
+++ systems-papers/python/network_citation/papers_statistics_graph.py
@@ -0,0 +1,91 @@
+#######################################
+# Variables of interest:
+# - gA:
+#     - outCitations
+# - gB:
+#     - inCitations
+#     - year
+
+# utilities
+import sys
+import os
+import numpy as np
+import utils.shared_utils as utils
+import utils.conf_utils as conf_utils
+import utils.colors as u_colors
+from tqdm import tqdm
+
+# modules
+import utils.data as u_data
+import semantic_scholar.s2data as s2data
+
+# matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
+np.random.seed(19680801)
+
+################################################################
+print("[#] Loading Data:")
+
+gA = s2data.get_dict_gA()
+gB = s2data.get_dict_gB()
+
+################################################################
+print("[#] Analyzing gA Data:")
+
+gA_statistics = {
+    "outCitations": []
+}
+ 
+for paper in tqdm(gA.values()):
+    # citation count
+    outcites = len(paper["outCitations"])
+    gA_statistics["outCitations"].append(outcites)
+
+################################################################
+print("[#] Analyzing gA Data:")
+
+gB_statistics = {
+    "inCitations": [],
+    "years": []
+}
+
+for paper in tqdm(gB.values()):
+    # citation count
+    incites = len(paper["inCitations"])
+    gB_statistics["inCitations"].append(incites)
+    # years
+    if "year" in paper: gB_statistics["years"].append(paper["year"])
+
+################################################################
+print("[#] Writing Results:")
+
+# gA : outCitations
+fig, ax = plt.subplots(tight_layout=True)
+data = np.array(gA_statistics["outCitations"])
+data = data / data.max() # normalize
+ax.hist(data , bins=n_bins)
+# TODO
+ax.vlines(x, ymin, ymax, colors='k', linestyles='solid', label='', *, data=None, **kwargs)
+# x : scalar or 1D array_like
+# x-indexes where to plot the lines.
+# ymin, ymax : scalar or 1D array_like
+# Respective beginning and end of each line. If scalars are provided, all lines will have same length.
+# colors : array_like of colors, optional, default: 'k'
+# linestyles : ['solid' | 'dashed' | 'dashdot' | 'dotted'], optional
+# label : string, optional, default: ''
+
+plt.show()
+quit()
+
+# gB : inCitations
+fig, ax = plt.subplots(tight_layout=True)
+data = np.array(gB_statistics["inCitations"])
+data = data / data.max() # normalize
+
+# gB : year
+fig, ax = plt.subplots(tight_layout=True)
+data = np.array(gB_statistics["year"])
+data = data / data.max() # normalize
