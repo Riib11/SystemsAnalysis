d470b0858e24bb72c56eea7979bcf569fdaab940
Henry <henry@reed.edu>
2018-08-13 18:44:37 -0700

    paper statistics

---
+++ systems-papers/python/network_citation/papers_statistics.py
@@ -0,0 +1,93 @@
+# utilities
+import sys
+import os
+import numpy as np
+import utils.shared_utils as utils
+import utils.conf_utils as conf_utils
+import utils.colors as u_colors
+from tqdm import tqdm
+
+# modules
+import utils.data as u_data
+import semantic_scholar.s2data as s2data
+
+def calcuate():
+
+    ################################################################
+    print("[#] Loading Data:")
+    
+    gA = s2data.get_dict_gA()
+    gB = s2data.get_dict_gB()
+
+    ################################################################
+    print("[#] Analyzing gA Data:")
+
+    def increment_entry(d, k):
+        if not k in d: d[k] = 0
+        d[k] += 1
+
+    gA_statistics = {
+        "outcitationcounts": [],
+        "titlewords": {}
+    }
+     
+    for paper in tqdm(gA.values()):
+        # citation count
+        outcitationcount = len(paper["outCitations"])
+        gA_statistics["outcitationcounts"].append(outcitationcount)
+        # title words
+        titlewords = paper["title"].split()
+        for word in titlewords:
+            increment_entry(gA_statistics["titlewords"],word)
+
+    ################################################################
+    print("[#] Analyzing gA Data:")
+
+    gB_statistics = {
+        "incitationcounts": [],
+        "titlewords": {}
+    }
+
+    for paper in tqdm(gB.values()):
+        # citation count
+        incitationcount = len(paper["inCitations"])
+        gB_statistics["incitationcounts"].append(incitationcount)
+        # title words
+        titlewords = paper["title"].split()
+        for word in titlewords:
+            increment_entry(gB_statistics["titlewords"],word)
+
+    ################################################################
+    print("[#] Writing Results:")
+
+    with open("papers_statistics.txt","w+") as file:
+        file.write("################################\n")
+        
+        ##############################
+        file.write("gA statistics:\n")
+        
+        file.write("  * outCitation counts:\n")
+        file.write("  |     mean : "+str(
+            np.mean(gA_statistics["outcitationcounts"]))+"\n")
+        file.write("  |   median : "+str(
+            np.median(gA_statistics["outcitationcounts"]))+"\n")
+        file.write("  |     mode : "+str(
+            gA_statistics["outcitationcounts"]))
+        file.write("  |      std : "+str(
+            np.std(gA_statistics["outcitationcounts"]))+"\n")
+
+        ##############################
+        file.write("gB statistics:\n")
+        
+        file.write("  * inCitation counts:\n")
+        file.write("  |     mean : "+str(
+            np.mean(gB_statistics["incitationcounts"]))+"\n")
+        file.write("  |   median : "+str(
+            np.median(gB_statistics["incitationcounts"]))+"\n")
+        file.write("  |     mode : "+str(
+            gB_statistics["incitationcounts"])+"\n")
+        file.write("  |      std : "+str(
+            np.std(gB_statistics["incitationcounts"]))+"\n")
+
+
+calcuate()
