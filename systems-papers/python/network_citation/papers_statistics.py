102c8d598307946296ba6ed8a09a29f15d3cf5e2
riib11 <blancheh@fries.reed.edu>
2018-08-14 09:43:27 -0700

    updates

--- systems-papers/python/network_citation/papers_statistics.py
+++ systems-papers/python/network_citation/papers_statistics.py
@@ -23,6 +23,7 @@ def calcuate():
     print("[#] Analyzing gA Data:")
 
     def increment_entry(d, k):
+        k = k.lower()
         if not k in d: d[k] = 0
         d[k] += 1
 
@@ -45,7 +46,8 @@ def calcuate():
 
     gB_statistics = {
         "incitationcounts": [],
-        "titlewords": {}
+        "titlewords": {},
+        "years": []
     }
 
     for paper in tqdm(gB.values()):
@@ -56,38 +58,57 @@ def calcuate():
         titlewords = paper["title"].split()
         for word in titlewords:
             increment_entry(gB_statistics["titlewords"],word)
+        # years
+        if "year" in paper:
+            gB_statistics["years"].append(paper["year"])
 
     ################################################################
     print("[#] Writing Results:")
 
     with open("papers_statistics.txt","w+") as file:
+        #
+        #
         file.write("################################\n")
-        
-        ##############################
         file.write("gA statistics:\n")
-        
-        file.write("  * outCitation counts:\n")
+        #
+        file.write("  + outCitation counts:\n")
         file.write("  |     mean : "+str(
             np.mean(gA_statistics["outcitationcounts"]))+"\n")
         file.write("  |   median : "+str(
             np.median(gA_statistics["outcitationcounts"]))+"\n")
-        file.write("  |     mode : "+str(
-            gA_statistics["outcitationcounts"]))
         file.write("  |      std : "+str(
             np.std(gA_statistics["outcitationcounts"]))+"\n")
+        #
+        file.write("  + top couple title words:\n")
+        def get(i): return i[1]
+        titlewords = sorted(list(gA_statistics["titlewords"].items()), key=get)
+        titlewords_len = len(titlewords)
+        for i in range(1,100+1): file.write("  |   "+str(i)+". ["+str(np.round(titlewords[-i][1]/titlewords_len, 3))+"] "+titlewords[-i][0]+"\n")
 
-        ##############################
+        #
+        #
+        file.write("################################\n")
         file.write("gB statistics:\n")
-        
-        file.write("  * inCitation counts:\n")
+        #        
+        file.write("  + inCitation counts:\n")
         file.write("  |     mean : "+str(
             np.mean(gB_statistics["incitationcounts"]))+"\n")
         file.write("  |   median : "+str(
             np.median(gB_statistics["incitationcounts"]))+"\n")
-        file.write("  |     mode : "+str(
-            gB_statistics["incitationcounts"])+"\n")
         file.write("  |      std : "+str(
             np.std(gB_statistics["incitationcounts"]))+"\n")
-
+        #
+        file.write("  + top couple title words:\n")
+        titlewords = sorted(list(gB_statistics["titlewords"].items()), key=get)
+        titlewords_len = len(titlewords)
+        for i in range(1,100+1): file.write("  |   "+str(i)+". ["+str(np.round(titlewords[-i][1]/titlewords_len, 3))+"] "+titlewords[-i][0]+"\n")
+        #
+        file.write("  + publish years:\n")
+        file.write("  |     mean : "+str(
+            np.mean(gB_statistics["years"]))+"\n")
+        file.write("  |   median : "+str(
+            np.median(gB_statistics["years"]))+"\n")
+        file.write("  |      std : "+str(
+            np.std(gB_statistics["years"]))+"\n")
 
 calcuate()
