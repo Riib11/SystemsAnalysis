37e931381f3d1b6be008e0eab968e8b64c5644ed
riib11 <blancheh@fries.reed.edu>
2018-08-13 17:26:19 -0700

    updates

--- systems-papers/notes/make_grep_missing_gA.py
+++ systems-papers/notes/make_grep_missing_gA.py
@@ -1,5 +1,17 @@
 with open("missing_ids_gA.txt", "r+") as ids:
     with open("grep_missing_gA.sh", "w+") as file:
-        for id in ids:
-            id = id.strip()
-            file.write("grep -h \"\\\"id\\\":\\\"" + id + "\\\"\" /data/sys-papers/semsch/*.json >> missing_gA.json\n")
+        batches = []
+        ids = [id.strip() for id in ids]
+        for i in range(len(ids)):
+            if i % 15 == 0:
+                batches.append([])
+            batches[-1].append(ids[i])
+
+        for batch in batches:
+            s = "grep -h "
+            for id in batch:
+                s += "-e '\"id\":\"" + id + "\"' "
+            s += "/data/sys-papers/semsch/*.json >> /home/blancheh/semsch/processed/missing_gA.json"
+            s += "\n"
+            file.write(s)
+
